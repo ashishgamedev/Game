2cde3ea7ac00fd2892252a2db1f1eae8e564c3b7
ashishgamedev <ashishkumarkaemail@gmail.com>
2020-09-14 09:33:04 +0530

    Add fuction is added.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "Game.h"
+
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
     std::cout << "First Commit" << std::endl;
+    std::cout << add(2, 3) << std::endl;
     return 0;
 }
